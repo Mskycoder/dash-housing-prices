8d0be0d0de8e02f8acca38f7662101c4957732a2
mskykiller <mskykiller@gmail.com>
2018-11-25 14:38:52 -0500

    heroku+tabs

--- utils.py
+++ utils.py
@@ -9,7 +9,4 @@ def StaticUrlPath(resource):
         raise Exception(dedent('''
             The file "{}" does not exist in the "static" folder.
         '''.format(resource, resource)))
-    if 'DYNO' in os.environ:
-        return '/{}/static/{}'.format(os.environ['DASH_APP_NAME'], resource)
-    else:
-        return '/static/{}'.format(resource)
+    return '/static/{}'.format(resource)
